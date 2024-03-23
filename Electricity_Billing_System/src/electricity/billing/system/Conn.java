17be251cb9834920f79c9ea77b64a6b3681cc749
palshuvam123 <140795981+palshuvam123@users.noreply.github.com>
2024-03-23 15:52:16 +0530

    Add files via upload

---
+++ Electricity_Billing_System/src/electricity/billing/system/Conn.java
@@ -0,0 +1,21 @@
+
+package electricity.billing.system;
+import java.sql.*;
+
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+    
+    Conn() {
+        try{
+      c =  DriverManager.getConnection("jdbc:mysql://localhost:3306/ebs","root","Shuvam@1234");
+     s = c.createStatement();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    
+}
+}
